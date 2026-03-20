bb35eacda281b67ae4f1c3bb5905e62b74f7050d
Alex Musayev <alex.musayev@gmail.com>
2026-03-20 16:56:45 +0300

    Add latest command to print absolute path to most recent note (#12)

---
+++ internal/cli/latest.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/dreikanter/notescli/note"
+	"github.com/spf13/cobra"
+)
+
+var latestCmd = &cobra.Command{
+	Use:   "latest [type]",
+	Short: "Print absolute path to the most recent note, optionally filtered by type",
+	Args:  cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		root := mustNotesPath()
+		notes, err := note.Scan(root)
+		if err != nil {
+			return err
+		}
+
+		if len(args) > 0 {
+			notes = note.FilterBySlug(notes, args[0])
+		}
+
+		if len(notes) == 0 {
+			if len(args) > 0 {
+				return fmt.Errorf("no notes found with type %q", args[0])
+			}
+			return fmt.Errorf("no notes found")
+		}
+
+		cmd.Println(filepath.Join(root, notes[0].RelPath))
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(latestCmd)
+}
